94a5c3a842ecc4d892d7241e43636f93bb881830
Ihsan Ahmed <ihsanahmedn569@gmail.com>
2026-07-16 18:04:19 -0400

    Configure GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://ihsanahmed6.github.io",
+  base: "/personal-portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
